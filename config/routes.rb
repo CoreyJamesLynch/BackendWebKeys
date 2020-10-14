d824cb18da010b7528b24356b22a76ad26dfc69e
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-14 19:46:39 -0400

    Add controllers to backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'playlists/index'
+  get 'playlists/show'
+  get 'playlists/create'
+  get 'playlists/update'
+  get 'playlists/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
+  get 'songs/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
