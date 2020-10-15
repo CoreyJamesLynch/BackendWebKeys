b4e41133691c47034d9d145eb7a8b201840fa462
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-15 14:22:13 -0400

    Change songs to sounds in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :playlists, only:[:index, :show, :create, :update, :destroy]
   resources :users, only:[:index, :show, :create]
-  resources :songs, only:[:index]
+  resources :sounds, only:[:index]
 end
